94af53241a5517d2bef1988b13d7a4e23b9918fa
Kai <kai@home.mc>
2016-04-09 23:27:14 -0400

    All ready. Parameters need changed to make perfect.

--- src/localizer_node.cpp
+++ src/localizer_node.cpp
@@ -10,14 +10,16 @@
 #include <tf/transform_listener.h>
 
 #define METRE_TO_PIXEL_SCALE 50
-#define NUM_PARTICLES 50
-#define FORWARD_SWIM_SPEED_SCALING 0.1
-#define MOTION_YAW_VARIANCE 0.05
-#define MOTION_TRANSLATION_VARIANCE 0.05
+#define NUM_PARTICLES 100
+#define FOCAL_HALF_WIDTH 20
+#define FORWARD_SWIM_SPEED_SCALING 0.07
 #define POSITION_GRAPHIC_RADIUS 20.0
 #define HEADING_GRAPHIC_LENGTH 50.0
 #define REEF_Z -7.0
 #define ROBOT_Z -5.0
+#define VARIANCE_MOTION_YAW .5
+#define VARIANCE_MOTION_FORWARD .05
+#define VARIANCE_PIXEL 10.
 
 class Particle
 {
@@ -84,6 +86,9 @@ public:
     ROS_INFO("Initialializing particles.");
     cumulativeWeights[0] = 0.0;
     for (int i = 0; i < NUM_PARTICLES; i++) {
+      double radius = normalRand(0.0, 10.0);
+      double theta = uniformRand(0.0, M_PI);
+      double yaw = uniformRand(0.0, VARIANCE_MOTION_YAW);
       particles[i].x = 0.0;
       particles[i].y = 0.0;
       particles[i].z = ROBOT_Z;
@@ -96,10 +101,7 @@ public:
   // To choose a particle index according to weights of the particles
   int pickIndex() {
     int low = 0, high = NUM_PARTICLES, mid;
-    std::mt19937 generator(std::random_device{}());
-    std::uniform_real_distribution<double> dist(0.0,
-                                            cumulativeWeights[NUM_PARTICLES]);
-    double random = dist(generator);
+    double random = uniformRand(0.0, cumulativeWeights[NUM_PARTICLES]);
 
     while (low + 1 < high) {
       mid = (low + high) / 2;   //certainly >= low + 1, and <= high - 1
@@ -116,7 +118,11 @@ public:
   // (I.e., probability of observing pixelToMatch given that
   //pixelGiven is the expected actual pixel value.)
   double match(cv::Vec3b const& pixelGiven, cv::Vec3b const& pixelToMatch) {
-    return 1.0;
+    return exp( -(double) (
+              (pixelToMatch[0]-pixelGiven[0])*(pixelToMatch[0]-pixelGiven[0]) +
+              (pixelToMatch[1]-pixelGiven[1])*(pixelToMatch[1]-pixelGiven[1]) +
+              (pixelToMatch[2]-pixelGiven[2])*(pixelToMatch[2]-pixelGiven[2])) /
+              (2. * VARIANCE_PIXEL * (4*FOCAL_HALF_WIDTH*FOCAL_HALF_WIDTH)));
   }
 
   // Pixel on the map corresponding to the point in the camera image.
@@ -180,20 +186,20 @@ public:
 
     cv::circle( localization_result_image,
         cv::Point(estimated_robo_image_x, estimated_robo_image_y),
-        POSITION_GRAPHIC_RADIUS/* *
-          particles[i].weight / cumulativeWeights[NUM_PARTICLES]*/,
+        4 /* POSITION_GRAPHIC_RADIUS *
+          particles[i].weight / cumulativeWeights[NUM_PARTICLES] */,
         CV_RGB(250,0,0), -1);
-    cv::line( localization_result_image,
+    /* cv::line( localization_result_image,
       cv::Point(estimated_robo_image_x, estimated_robo_image_y),
       cv::Point(estimated_heading_image_x, estimated_heading_image_y),
-      CV_RGB(250,0,0), 10);
+      CV_RGB(250,0,0), 10); */
   }
 
+  // Camera image callback
   void robotImageCallback( const sensor_msgs::ImageConstPtr& robot_img )
   {
-    // TODO: You must fill in the code here to implement an observation model for your localizer
-
     cv_bridge::CvImagePtr cv_ptr;
+    cv::Mat cameraImage;
     try
     {
       cv_ptr = cv_bridge::toCvCopy(robot_img, sensor_msgs::image_encodings::BGR8);
@@ -204,11 +210,36 @@ public:
       return;
     }
 
-    localization_result_image = cv::Mat(cv_ptr->image);
+    cameraImage = cv::Mat(cv_ptr->image);
+    int xStart = cameraImage.size().width/2 - FOCAL_HALF_WIDTH;
+    int xEnd = cameraImage.size().width/2 + FOCAL_HALF_WIDTH;
+    int yStart = cameraImage.size().height/2 - FOCAL_HALF_WIDTH;
+    int yEnd = cameraImage.size().height/2 + FOCAL_HALF_WIDTH;
 
-    buildEstimatesCameraImage(&localization_result_image, particles[0]);
+    //Propagate observation model: resample, and update weights.
+    Particle newParticles[NUM_PARTICLES];
+    for (int i = 0; i < NUM_PARTICLES; i++) {
+      newParticles[i] = particles[pickIndex()];
+    }
+    for (int i = 0; i < NUM_PARTICLES; i++) {
+      particles[i] = newParticles[i];
+      double newWeight = 1.0;
+      for (int xI = xStart; xI < xEnd; xI++) {
+        for (int yI = yStart; yI < yEnd; yI++) {
+          newWeight*= match(imagePointToMapPixel(xI, yI, particles[i]),
+                                  cameraImage.at<cv::Vec3b>(yI, xI));
+        }
+      }
+      particles[i].weight = newWeight;
+      cumulativeWeights[i+1] = cumulativeWeights[i] + particles[i].weight;
+    }
+
+    /* localization_result_image = cv::Mat(cv_ptr->image);
+    buildEstimatesCameraImage(&localization_result_image, particles[0]); */
   }
 
+  // Helper to rebuild camera image given estimated pose
+  // Used to test whether imagePointToMapPixel is working correctly
   void buildEstimatesCameraImage(cv::Mat* image_ptr, Particle particle) {
     for (int xI = 0; xI < image_ptr->size().width; xI++)
       for (int yI = 0; yI < image_ptr->size().height; yI++) {
@@ -218,25 +249,7 @@ public:
 
   }
 
-  // Function motionCommandCallback is a example of how to work with Aqua's motion commands (your view on the odometry).
-  // The initial version simply integrates the commands over time to make a very rough location estimate.
-  // TODO: You must improve it to work with the localizer you implement.
-  //
-  // Note the somewhat unique meaning of fields in motion_command
-  //    motion_command
-  //      pose
-  //        position
-  //          x - requested forward swim effort in a unitless number ranging from 0.0 to 1.0. You must translate this into
-  //              a velocity estimate in some way. Currently a simple constant is used.
-  //          y - requested up/down swim effort. Not used in this assignment
-  //          z - unused
-  //        orientation - A quaternion that represents the desired body orientation w.r.t. global frame. Note that
-  //                      Aqua's controller will not achieve this orientation perfectly, so you must consider that
-  //                      this is only a noisy estimate of the robot's orientation (correct for it with your filter!)
-  //
-  // Note that we use negative angles because the geometry of the map image is formed with its Z axis pointing downwards
-  // (the camera is looking at the ocean floor)
-  //
+  // Motion command callback
   void motionCommandCallback(const geometry_msgs::PoseStamped::ConstPtr& motion_command )
   {
 
@@ -250,11 +263,11 @@ public:
     updateParticles(command.pose.position.x, target_yaw);
 
     // Comment the one following line to plot your whole trajectory without ground truth
-    //localization_result_image = ground_truth_image.clone();
+    localization_result_image = ground_truth_image.clone();
 
     //Draw particles on result image.
     for (int i = 0; i < NUM_PARTICLES; i++) {
-      //drawParticleOnMap(i);
+      drawParticleOnMap(i);
     }
   }
 
@@ -288,22 +301,28 @@ public:
     }
   }
 
+  // Random value according to uniform distribution
+  static double uniformRand(const double low, const double high) {
+    std::mt19937 generator(std::random_device{}());
+    std::uniform_real_distribution<double> dist(low, high);
+    return dist(generator);
+  }
+
   // Random value according to normal distribution
   static double normalRand(const double mean, const double variance) {
     std::mt19937 generator(std::random_device{}());
     std::normal_distribution<double> distribution(mean, variance);
-    //return distribution(generator);
-    return mean;
+    return distribution(generator);
   }
 
   // Function to return a yaw according to some chosen distribution, given mean
   static double probableYaw(const double mean) {
-    return normalRand(mean, MOTION_YAW_VARIANCE);
+    return normalRand(mean, VARIANCE_MOTION_YAW);
   }
 
   // Function to return a probable translation value
   static double probableTranslation(const double mean) {
-    return normalRand(mean, MOTION_TRANSLATION_VARIANCE);
+    return normalRand(mean, VARIANCE_MOTION_FORWARD);
   }
 
 
